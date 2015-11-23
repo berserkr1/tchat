7567e912118677529c414870da9a5824c987c156
berserkr1 <g.coirier@epmistes.net>
2015-11-23 11:39:52 +0100

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$db = mysqli_connect('localhost', 'root', 'troiswa', 'blog');
+if ($db === false)
+	die(mysqli_connect_error()); 
+	$errors = new array();
+	$access = array('accueil', 'login', 'logout', 'register', 'tchat');
+	$traitements = array('login', 'logout', 'register');
+	$accessAdmin = array('admin', 'accueil', 'login', 'logout', 'register', 'tchat');
+	$traitementsAdmin = array('admin', 'login', 'logout', 'register', 'tchat');
+	$page = 'accueil';
+	if (isset($_GET['page']))
+	{
+		if (in_array($_GET['page'], $traitements)) {
+			require('apps/traitement_'.$_GET['page'].'.php');
+		}
+		else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $traitementsAdmin))
+		{
+		require('apps/traitement_'.$_GET['page'].'.php');
+		}
+		if (in_array($_GET['page'], $access)) {
+			$page = $_GET['page'];
+		}
+		else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $accessAdmin))
+		{
+		$page = $_GET['page'];
+		}
+	}
+	require('apps/skel.php');
+?>
---
+++ tchat.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Lun 23 Novembre 2015 à 11:31
+-- Version du serveur: 5.5.44-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `tchat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `contenu` varchar(1022) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(31) COLLATE utf8_bin NOT NULL,
+  `avatar` varchar(511) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
