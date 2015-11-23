dccd9d35eb11aec4394915913123d993ea4bd3da
Somewher <mickaelkael12@hotmail.com>
2015-11-23 15:21:57 +0100

    maj register

--- apps/traitement_register.php
+++ apps/traitement_register.php
@@ -1,29 +1,38 @@
 <?php
-	$pseudo = $mdp = $mdp2 = "";
-	$errors = "";
-	if (isset($_POST['pseudo'], $_POST['mdp'],$_POST['mdp2'])) {
-		$pseudo = mysqli_real_escape_string($db, $_POST['pseudo']);
-		$mdp = mysqli_real_escape_string($db, $_POST['mdp']);
-		$mdp2 = $_POST['mdp2'];
-		$avatar = mysqli_real_escape_string($db, $_POST['avatar']);
-		if ($mdp != $mdp2)
+$pseudo = "";
+$mdp1 = "";
+$mdp2 = "";
+if (isset($_POST['login'], $_POST['mdp1'], $_POST['mdp2']))
+{
+	$login = mysqli_real_escape_string($db, $_POST['pseudo']);
+	$mdp1 = $_POST['mdp1'];
+	$mdp2 = $_POST['mdp2'];
+	if ($mdp1 != $mdp2)
+	{
+		$errors[] = "Password don't match";
+	}
+	else if (strlen($mdp1) < 6)
+	{
+		$errors[] = "Password too short";
+	}
+	if (strlen($pseudo) < 4)
+	{
+		$errors[] = "Login too short";
+	}
+	if (count($errors) == 0)
+	{
+		$hash = password_hash($mdp1, PASSWORD_BCRYPT, array("cost"=>10));
+		$query = "INSERT INTO user (pseudo, password) VALUES('". $pseudo ."', '". $hash ."')";
+		$resultat = mysqli_query($db, $query);
+		if ($resultat)
 		{
-			$errors[] = "Password don't match";
-		}
-		else if (strlen($mdp) < 6)
-		{
-			$errors[] = "Password too short";
-		}
-		if (strlen($pseudo) < 4)
-		{
-			$errors[] = "Login too short";
-		}
-		if (count($errors) == 0) {
-			require('apps/password.php');
-			$query = "INSERT INTO user (pseudo,password,avatar) VALUES ('".$pseudo."','".$mdp."','".$avatar."')";
-			mysqli_query($db,$query);
-			header('Location: index.php?page=register');
+			header('Location: index.php?page=login');
 			exit;
 		}
+		else
+		{
+			$errors[] = mysqli_error($db);
+		}
 	}
+}
 ?>
--- index.php
+++ index.php
@@ -3,27 +3,34 @@ session_start();
 $db = mysqli_connect('192.168.1.84', 'mickael', '', 'tchat');
 if ($db === false)
 	die(mysqli_connect_error()); 	
-	$access = array('accueil', 'login', 'logout', 'register');
-	$traitements = array('login', 'logout', 'register');
-	$accessAdmin = array('admin', 'accueil', 'login', 'logout', 'register');
-	$traitementsAdmin = array('admin', 'login', 'logout', 'register');
-	$page = 'accueil';
-	if (isset($_GET['page']))
+$errors = array();
+
+$access = array('accueil', 'login', 'logout', 'register');
+$traitements = array('login', 'logout', 'register');
+
+$accessAdmin = array('admin', 'accueil', 'login', 'logout', 'register');
+$traitementsAdmin = array('admin', 'login', 'logout', 'register');
+
+$page = 'accueil';
+
+if (isset($_GET['page']))
+{
+	if (in_array($_GET['page'], $traitements)) 
 	{
-		if (in_array($_GET['page'], $traitements)) {
-			require('apps/traitement_'.$_GET['page'].'.php');
-		}
-		else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $traitementsAdmin))
-		{
-		require('apps/traitement_'.$_GET['page'].'.php');
-		}
-		if (in_array($_GET['page'], $access)) {
-			$page = $_GET['page'];
-		}
-		else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $accessAdmin))
-		{
-		$page = $_GET['page'];
-		}
+		require('apps/traitement_'. $_GET['page'] .'.php');
 	}
-	require('apps/skel.php');
+	else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $traitementsAdmin))
+	{
+		require('apps/traitement_'. $_GET['page'] .'.php');
+	}
+	if (in_array($_GET['page'], $access)) 
+	{
+		$page = $_GET['page'];
+	}
+	else if (isset($_SESSION['id']) && $_SESSION['admin'] === true && in_array($_GET['page'], $accessAdmin))
+	{
+		$page = $_GET['page'];
+	}
+}
+require('apps/skel.php');
 ?>
