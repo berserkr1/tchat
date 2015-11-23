b4c350df865e127fcafe5721ea77c76cd4adc5be
Somewher <mickaelkael12@hotmail.com>
2015-11-23 13:10:51 +0100

    index

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 session_start();
 $db = mysqli_connect('localhost', 'root', 'troiswa', 'tchat');
 if ($db === false)
-	die(mysqli_connect_error()); 
-	$errors = new array();
+	die(mysqli_connect_error()); 	
 	$access = array('accueil', 'login', 'logout', 'register');
 	$traitements = array('login', 'logout', 'register');
 	$accessAdmin = array('admin', 'accueil', 'login', 'logout', 'register');
