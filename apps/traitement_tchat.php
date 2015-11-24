0ade890f503a0214aa3fb87ac337f0f7240d8b5c
Somewher <mickaelkael12@hotmail.com>
2015-11-24 11:28:46 +0100

    tchat

--- apps/traitement_tchat.php
+++ apps/traitement_tchat.php
@@ -1,13 +1,18 @@
 <?php 
-	$post ="";
+	$post = "";
+	$query = "SELECT post.id_user, user.id
+			  FROM post
+			  LEFT JOIN user ON post.id_user = user.id";
+	$resultat = mysqli_query($db, $query);
+	$user = mysqli_fetch_assoc($resultat);
 	if (isset($_POST['post']))
 	{
 		$post = mysqli_real_escape_string($db, $_POST['post']);
-		$query = "INSERT INTO post (contenu) VALUES('". $post ."')";
-		$resultat = mysqli_query($db, $query);
+		$query2 = "INSERT INTO `tchat`.`post` (`id`, `id_user`, `contenu`, `date`) VALUES (NULL, '" . $user['id'] . "', '" . $post . "', CURRENT_TIMESTAMP)";
+		$resultat = mysqli_query($db, $query2);
 		if ($resultat)
 		{
-			header('Location: index.php?page=login');
+			header('Location: index.php?page=accueil');
 			exit;
 		}
 		else
