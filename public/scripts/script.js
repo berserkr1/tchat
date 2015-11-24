df39355295c8a94c5a64e21d46d90538e374342f
Somewher <mickaelkael12@hotmail.com>
2015-11-24 13:17:52 +0100

    popup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,10 @@
-$(document).ready( function () {
+$(document).ready(function() {
 	$("#formulaire").submit(function(e) {
-		e.preventDefault();
-		alert(''+ this.post + "");
-        } );
-} );
+		e.preventDefault();		
+        });
+});
+
+function controle(form1) {
+var post = document.form1.input.value;
+alert(post);
+}
