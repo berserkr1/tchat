f9a54590db0b92910cb45332c9b672e6a29c51d8
Berserkr1 <g.coirier@epmistes.net>
2015-11-25 02:28:08 +0100

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,23 +2,44 @@
 tchatBox = document.getElementById('tchat-read');
 tchatBox.scrollTop=tchatBox.scrollHeight;
 
-// $('document').ready(function()
-// {
-		function envoiMessage(form) {
-			if (form.message != "") { return true; }
-			else { return false; }
-	 	//, function(message)
-	// 	{
-	// 		var tab = JSON.parse(message);
-	// 		$('.tchat-read').empty();
-	// 		var i = 0;
-	// 		while (i < tab.length)
-	// 		{
-	// 			$('.tchat-read').append('<p>'+tab[i]+'</p>');
-	// 			i++;
-	// 		}
-	// 	});
-		}/*)*/;
-// });
+function messages() {
+		global $db;
+		$query = "SELECT * FROM post LEFT JOIN user ON post.id_user = user.id ORDER BY 'date'";
+		$resultat = mysqli_query($db, $query);
+		if ($resultat) {
+			while ($message = mysqli_fetch_assoc($resultat)) {
+				echo "<".$message['date']."> ".htmlentities($message['pseudo'])." : ".htmlentities($message['contenu'])."<br/>";
+			}
+		}
+	}
 
-// setTimeout() pour le refresh
+function charger()
+	{
+		setInterval(function()
+		{
+			$.get('', function(page)
+			{
+				$('section').html(page);
+			});
+		}, 1000);
+	}
+
+$(document).ready(function() {
+	$("#tchat-form").submit(function(event) {
+		event.preventDefault();
+		var message = $('#tchat-write').val();
+		if(message != "") {			
+			$.ajax 
+			({
+				url : "index.php?page=accueil",
+				type : "POST",
+				data:{"message":message},
+				success : function(html) {
+					$('#tchat-read').prepend(html);
+				}
+			});	
+			charger();			
+		}
+	});
+});
+}
