5ec03524cacc125e0352fb6bd2ef046924b9c83e
Berserkr1 <g.coirier@epmistes.net>
2015-11-25 02:21:10 +0100

    Update accueil.php

--- apps/accueil.php
+++ apps/accueil.php
@@ -3,14 +3,15 @@
 	if (isset ($_SESSION['id'])) {
 		if (isset($_POST['ajax'])) {
 			if(isset($_SESSION['id'], $_POST['message']) && $_POST['message'] != "") {
-			$message = mysqli_real_escape_string($db, $_POST['message']);
-			$query = "INSERT INTO post (id_user,contenu) VALUES ('".intval($_SESSION['id'])."','".$message."')";
-			$resultat = mysqli_query($db, $query);
-			if ($resultat) {
-				$message="";
-			}
-			else {
-				$errors[]="Problème de connexion au serveur de messagerie";
+				$message = mysqli_real_escape_string($db, $_POST['message']);
+				$query = "INSERT INTO post (id_user,contenu) VALUES ('".intval($_SESSION['id'])."','".$message."')";
+				$resultat = mysqli_query($db, $query);
+				if ($resultat) {
+					$message="";
+				}
+				else {
+					$errors[]="Problème de connexion au serveur de messagerie";
+				}
 			}
 		}
 		require('views/tchat.phtml');
