ed46e0161e9520474c7eb3c2cd104384db164edc
Somewher <mickaelkael12@hotmail.com>
2015-11-25 12:45:45 +0100

    logout

--- apps/traitement_tchat.php
+++ apps/traitement_tchat.php
@@ -1,11 +1,12 @@
 <?php 
+
 	$post = "";
 	$query = "SELECT post.id_user, user.id
 			  FROM post
 			  LEFT JOIN user ON post.id_user = user.id";
 	$resultat = mysqli_query($db, $query);
 	$user = mysqli_fetch_assoc($resultat);
-	if (isset($_POST['input']))
+	if (isset($_POST['input'], $_SESSION['id']))
 	{
 		$post = mysqli_real_escape_string($db, $_POST['input']);
 		$query2 = "INSERT INTO `tchat`.`post` (`id`, `id_user`, `contenu`, `date`) VALUES (NULL, '" . $_SESSION['id'] . "', '" . $post . "', CURRENT_TIMESTAMP)";
